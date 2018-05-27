df2385cfdba28fc3486a4a01b43e2eee2438af36
tarrsalah <tarrsalah@gmail.com>
2018-05-27 01:52:05 +0100

    working.

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+const m = () => (req, res, next) => {
+  next();
+};
+
+app.use(m());
+
+app.get("/", (req, res) => {
+  res.sendStatus(200);
+});
+
+app.listen(5000);
